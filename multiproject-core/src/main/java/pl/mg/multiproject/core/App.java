b4d0a06faf81972fa5ff639e8f992cde70ec6032
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2016-03-25 15:55:37 +0100

    Initial commit.

---
+++ multiproject-core/src/main/java/pl/mg/multiproject/core/App.java
@@ -0,0 +1,15 @@
+package pl.mg.multiproject.core;
+
+import pl.mg.multiproject.extension.Test;
+
+/**
+ * Created by maciej on 25.03.16.
+ */
+public class App {
+
+    public static void main(String[] args){
+        Test.test();
+
+    }
+
+}
---
+++ multiproject-extension/src/main/java/pl/mg/multiproject/extension/Test.java
@@ -0,0 +1,12 @@
+package pl.mg.multiproject.extension;
+
+/**
+ * Created by maciej on 25.03.16.
+ */
+public class Test {
+
+    public static void test(){
+        System.out.println("hello world");
+    }
+
+}
